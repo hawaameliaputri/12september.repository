b5f1ff5dc5d4f493448725ca4370987d6f081c3c
hawaameliaputri <143465715+hawaameliaputri@users.noreply.github.com>
2023-09-14 07:35:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const judul = document.getElementById('judul');
+judul.style.color='red';
+judul.style.backgroundColor = 'black';
+judul.innerHTML='Amelia';
+
+const p = document.getElementsByTagName('p');
+//p[0].style.backgroundColor='red';
+//p[1].style.backgroundColor='red';
+//p[2].style.backgroundColor='red';
+//p[3].style.backgroundColor='red';
+
+const h1 = document.getElementsByTagName('h1');
+h1[0].style.fontSize='50px';
+
+const p1 = document.getElementsByClassName('p1');
+p1[0].innerHTML= "dirubah dengan javascript";
+
+cons p4 = document.querySelector.apply('#bb p');
+p4.style.color = 'green';
+p4.style.fontSize= '25px';
+
+const li2 = document.querySelector('li:ncth-child(2)')
+li2.style.backgroundColor
